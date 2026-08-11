c6c845aed8d0a6d832d730adcf9423ff63fb05eb
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-11 23:46:48 +0800

    used angular material for navbar component

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [RouterLink],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+    selector: 'app-navbar',
+    imports: [
+        RouterLink,
+        MatToolbarModule,
+        MatButtonModule,
+        MatIconModule,
+        MatMenuModule
+    ],
+    templateUrl: './navbar.html',
+    styleUrl: './navbar.scss',
 })
 export class Navbar {}
