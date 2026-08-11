61085fec31d9f740f70b661a8466d6a9ace2f3d4
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-11 19:46:38 +0800

    Created Register Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Register
+    },
+    {
+        path: 'register',
+        component: Register
+    }
+];
