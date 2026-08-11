8e7e3b6344216073ba3d7f7d038022cd04e75f45
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-11 20:01:08 +0800

    Created Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Register
+        component: Home
     },
     {
         path: 'register',
