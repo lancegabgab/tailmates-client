d30fbae4b39db2b9b1f92628765a64ef1ad4c484
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-20 19:15:34 +0800

    Updated Login page
    used Angular Material

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
