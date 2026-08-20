bc42114ff10219bcbbd405b05bf2476b0b58d2f4
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-08-20 19:10:34 +0800

    Updated Register page
    used Angular Material

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
