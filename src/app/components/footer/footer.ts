47e09354cd268a082ae092d343403fc778bec650
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-09-01 06:28:06 +0800

    Updated Footer
    replaced hardcoded year

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -7,4 +7,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
-export class Footer {}
+export class Footer {
+  currentYear = new Date().getFullYear();
+}
